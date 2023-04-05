c2a2b50d5b225571695909cf8f227c041fc2255b
Ousmane Konate <konatealim@gmail.com>
2023-04-05 18:14:49 +0200

    fixing errors

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- creates a table users
 Create table if not exists users (
     id int not null auto_increment,
-    name varchar(255) not null,
     email varchar(255) not null,
+    name varchar(255),
     primary key (id),
     unique key (email)
 );
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 Create table if not exists users (
     id int not null auto_increment,
     email varchar(255) not null,
-    name varchar(255) not null,
+    name varchar(255),
     country enum('US', 'CO', 'TN') not null default 'US',
     primary key (id),
     unique key (email)
