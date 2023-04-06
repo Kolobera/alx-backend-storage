9f553c011e96c3b46e46f38eace4653b10483382
Ousmane Konate <konatealim@gmail.com>
2023-04-06 16:45:22 +0200

    fixing errors

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 --  lists all bands with Glam rock as their main style, ranked by their longevity
-select band_name, (ifnull(split, '2020') - formed) as lifespan
-from metal_bands
-where find_in_set('Glam rock', ifnull(style, "")) > 0
-order by lifespan desc;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
