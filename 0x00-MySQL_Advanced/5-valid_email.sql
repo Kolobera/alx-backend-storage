6765c57991d2382fdde37b91f84b55cde571f778
Ousmane Konate <konatealim@gmail.com>
2023-04-06 17:16:09 +0200

    add task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- trigger to resets the attribute valid_email only when the email has been changed
+delimiter //
+create trigger reset_valid_email
+before update on users
+for each row
+begin
+    if old.email != new.email then
+        set new.valid_email = 0;
+    end if;
+end //
+delimiter ;
