61ef67eb0858e82cdeb48372d63a6d47a1f87e62
Ousmane Konate <konatealim@gmail.com>
2023-04-08 21:24:05 +0200

    add task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,6 +2,6 @@
 drop view if exists need_meeting;
 create view need_meeting as
 select u.name
-from users u
+from students u
 where u.average_score < 80 
 and (u.last_meeting is null or u.last_meeting < date_sub(now(), interval 1 month));
