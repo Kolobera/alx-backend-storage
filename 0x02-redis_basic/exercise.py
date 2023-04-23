9faa27d82a1adb88a49690feb47baa0112ffe589
Ousmane Konate <konatealim@gmail.com>
2023-04-23 23:36:54 +0200

    add task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Task 0's module.
+"""
+import redis
+
+
+class Cache:
+    """A Cache class.
+    """
+    def __init__(self):
+        """Initializes the class.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        """Stores data in Redis using a random key.
+        """
+        key = self._redis.randomkey()
+        self._redis.set(key, data)
+        return key
