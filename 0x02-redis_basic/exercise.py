236e984ba0c9013a64eb63ea8b2840f73d264e8d
Ousmane Konate <konatealim@gmail.com>
2023-04-23 23:46:35 +0200

    Implementing get functions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,21 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Gets data from Redis.
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> str:
+        """Gets data from Redis as a string.
+        """
+        return self.get(key, str)
+    
+    def get_int(self, key: str) -> int:
+        """Gets data from Redis as an integer.
+        """
+        return self.get(key, int)
